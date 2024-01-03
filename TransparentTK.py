a1bb4f664fadfeae3849dfbe0284b0e920a8504b
Edm1795 <aswitzer@ualberta.ca>
2024-01-02 23:32:33 -0700

    Create TransparentTK.py

---
+++ TransparentTK.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+# Create object
+root = Tk()
+
+# Adjust size
+root.geometry("400x400")
+
+# Create transparent window
+root.attributes('-alpha',0.5)
+
+# Execute tkinter
+root.mainloop()
