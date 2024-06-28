dda37ab8589bb0578d9a371609737b2d8176499b
Edm1795 <aswitzer@ualberta.ca>
2024-06-27 21:03:57 -0600

    Update TransparentTK.py

--- TransparentTK.py
+++ TransparentTK.py
@@ -5,14 +5,18 @@ from tkinter import *
 root = Tk()
 
 # Adjust size
-root.geometry("400x400")
+root.geometry("600x500")
 
 # Create transparent window
 root.attributes('-alpha',0.5)
 
+x1,x2=400,400
+y1=100
+y2=200
+
 canvas=Canvas(root)
 canvas.pack(pady=0,padx=0,fill=BOTH, expand=True)
-canvas.create_line(x1,y1,x2,y2,width='',fill='green')
+canvas.create_line(x1,y1,x2,y2,width='10',fill='green')
 
 # Execute tkinter
 root.mainloop() # do not run this line if running this in a console realtime mode
