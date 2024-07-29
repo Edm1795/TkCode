bb219efc666ea54ea8aaf66793035aba2db08dd1
Edm1795 <aswitzer@ualberta.ca>
2024-07-28 21:57:46 -0600

    Update TimeLine.py Correcting Mistaken Commit
    The previous commit was for TK not for Timeline. This therefore reverts back to the last point of timeline.

--- TimeLine.py
+++ TimeLine.py
@@ -1,14 +1,10 @@
 # colour picker crtl shift a, Type in colour
-import time
 
-import DFQuickCheck
-import TimeLine
+import time
+from datetime import datetime
 from tkinter import *
-from ScreenShot import *
 from ctypes import windll  # used for fixing blurry fonts on win 10 and 11 (also  windll.shcore.SetProcessDpiAwareness(1))
 #from tkinter import ttk
-
-# Import pyscreeze 1.29 to pycharm, not directly into this module, otherwise another module related to pyscreeze (not sure how) will raise an exception if searching for the image on screen takes a little time
 
 
 
@@ -19,154 +15,104 @@ class MainWindow:
         # Master Window
         self.master = master
         self.master.title('AutoGui Ver. 0.0')
-        self.master.geometry("+150+500")  # position of the window in the screen (200x300) ("-3300+500")
-        self.master.geometry("400x200")  # set initial size of the root window (master) (1500x700);
+        self.master.geometry("+150+500")  # position of the window in the screen (200x300)
+        self.master.geometry("1000x400")  # set initial size of the root window (master) (1500x700);
         # if not set, the frames will fill the master window
-        # self.master.attributes('-fullscreen', True)
+        self.master.attributes('-fullscreen', True)
         screenWidth = self.master.winfo_screenwidth()
         screenHeight = self.master.winfo_screenheight()
+        self.master.config(bg='white') # use this in conj with wm_atts below bg="#add123"
 
         self.master.attributes("-topmost", True)
+        # self.master.attributes('-alpha', 0.2) # make window transparent 1.0=totally solid
+        self.master.wm_attributes('-transparentcolor','white') # make viewable window transp, not the top bar
 
-        # Instantiate frames
-        self.frame0 = Frame(self.master, bd=5, padx=5, bg='#606266')  # Top long row
-        self.frame1 = Frame(self.master, bd=5, padx=5, bg='#2a2b2b')  # Side Column
-        self.frame2 = Frame(self.master, bd=5, padx=5, bg='#7E050C')  # Main frame
+        # highlibackgrnd--just like a border, make white so it goes transparent. This makes the canvas completely transparent but not anything on it which is other than white
+        canvas = Canvas(self.master,width=screenWidth,height=screenHeight, bg="white",highlightbackground="white")
+        #canvas.create_line(15, 0, 15, 500, width=5) # x,y x,y, dash=(10)
+        canvas.pack(pady=0,padx=0,fill=BOTH, expand=True)
 
-        # Place frames
-        self.frame0.grid(row=0, column=0, columnspan=2, sticky="nsew")
-        self.frame1.grid(row=1, column=0, columnspan=1, sticky="nsew")
-        self.frame2.grid(row=1, column=1, columnspan=1, sticky="nsew")
+        self.timeLine(canvas)
 
-        # configure weighting of frames
-        self.master.grid_columnconfigure(0,
-                                         weight=1)  # First int refers to column numberAllows frames to expand as master window expands; weight tells how much of the columns it takes
-        self.master.grid_columnconfigure(1, weight=7)  # weight gives 3 times as much column as the other columns
-        # self.master.grid_columnconfigure(2, weight=1)
-        self.master.grid_rowconfigure(1, weight=1)  # rowconfigure states: first row takes 1 parts of space
+        # add button directly on to screen for closing the timeline
+        self.button = Button(canvas, text="Close Timeline", width=12, command=close)
+        self.button.pack()
 
-        self.frame1.grid_propagate(0)  # When adding widgets maintain weighting of frames
-        self.frame2.grid_propagate(0)
 
-        # sundries
-        self.counter=0 # counter used for determining state of toggle switch used in transparency button
 
-        frameWidth = 10  # Units are in characters not pixels
+
+
+    def timeLine(self,canvas):
+        '''
+        Draws a line downwards at the correct current time on screen. This function has a helper function
+        which calulates the time difference from 8 am to the current time of day
+        inputs: the canvas
+        '''
+
+        def getTimeDiff():
+            '''
+            This function calculates the number of minutes elapsed between 8 am and the current time of day
+            Ex: if it is 9 am currently, then 60 mins has elapsed since 8 am
+            :return: float of num of minutes elapsed
+            '''
+
+            # t = time.localtime() # get local time
+            # current_time = time.strftime("%H:%M:%S", t) # convert local time to string readable
+
+            now = datetime.now()
+            currTime = now.strftime("%H:%M:%S")
+
+            start = datetime.strptime("8:00:00", "%H:%M:%S")
+            end = datetime.strptime(currTime, "%H:%M:%S")
+
+            difference = end - start
+
+            seconds = difference.total_seconds()
+
+            # hours = seconds / (60 * 60)
+
+            return seconds / 60 # returns number of minutes elapsed
+
+
+        # 30 mins = how many pixels? --> 831-794 = 37
+        # 37 pixels / 30 = 1.2333 pixels per minute
+
+        # 1.2333.. number of pixels horizontally per minute of time, move timeline 1.23 pixels every minute
+        x1,x2 = (getTimeDiff() * 1.24) + 718, (getTimeDiff() * 1.24) + 718 # (Use 523 as 8 am start value, and 1.00 as pixel pur minute for w com.) x coords = num of minutes elapsed since 8 am * num of pixels per minute (1.23)
+        y1=380
+        y2=900
+        # x1, y1, x2, y2 = 718,423,718, 900 # 8:00am top point: 718,423. # 9am top point: 795,431   795, 431, 795, 900
+        # 9 am ? home vals: 795, 431, 795, 900; work vals: 524,280,524,900
+        canvas.create_line(x1, y1, x2, y2, width=1, fill="green") # draw a line at coordinates needed to display at correct time on schedule
+
+        # this loop can be used to draw a line, then erase that original line and draw a new line.
+        # loop=False
+        # while loop:
+        #     canvas.create_line(x1,y1,x2,y2,width=1,fill="green")
+        #     self.master.update()
+        #     time.sleep(0.2)
+        #     canvas.create_line(x1, y1, x2, y2, width=2, fill="#FFFFFF")
+        #     self.master.update()
+        #     x1+=1
+        #     x2+=1
+
+
+
+
+
+
 
         windll.shcore.SetProcessDpiAwareness(1)  # used for fixing blurry fonts on win 10 and 11
 
-
-        #  Entry widgets
-        self.entry = Entry(self.frame0, width=10)
-        self.entry.pack()
-
-        #self.photo = PhotoImage(file="Speedometer2.png")
-        #self.master.iconphoto(False, self.photo)
-
-
-        self.button = Button(self.frame1, text="Schedule", width=12, command=self.schedule)
-        self.button.pack()
-
-        self.button = Button(self.frame1, text="Screen Shot", width=12, command=self.screenShot)
-        self.button.pack()
-
-        self.button = Button(self.frame1, text="Show Timeline", width=12, command=self.openTimeLine)
-        self.button.pack()
-
-        self.button = Button(self.frame1, text="Close Timeline", width=12, command=self.closeTimeLine)
-        self.button.pack()
-
-        self.button = Button(self.frame1, text="Transparent", width=12, command=self.makeWinTrans)
-        self.button.pack()
-
-        self.selected = StringVar() # selected is a fucntion with methods, use selected.get() to get the string val
-        self.rad1 = Radiobutton(self.frame2, text='LAs', value='1', variable=self.selected) # ret 1 if desiring LAs
-        self.rad2 = Radiobutton(self.frame2, text='Pages', value='2', variable=self.selected) # ret 2 if desiring Pages
-        self.rad1.pack()
-        self.rad2.pack()
-
-        self.addItemButton = Button(self.frame1, text="Create New", width=12, command=self.createNew)
-        self.addItemButton.pack()
-        #
-        # self.clearFramesButton = Button(self.frame1, text="Clear Frames", width=12,
-        #                                 command=self.clearSpeedoFrame)  # Clears frame2, the speedometer frame of all widgets
-        # self.clearFramesButton.pack()
-        #
-        # self.showHistoryButton = Button(self.frame1, text="History", width=12,
-        #                                 command=showHistory)  # Clears frame2, the speedometer frame of all widgets
-        # self.showHistoryButton.pack()
-
-    def schedule(self):
-        DFQuickCheck.main()
-        TimeLine.main()
-
-    def setCount(self):
-        self.counter+=1
-
-    def closeTimeLine(self):
-        try:
-            TimeLine.close()
-        except:
-            pass
-
-    def openTimeLine(self):
-        # Closes the currently showing timeline and re-runs timeline for the current time (the updated time)
-        try:
-            TimeLine.close()
-        except:
-            TimeLine.main()
-        else:
-            TimeLine.main()
-
-    def makeWinTrans(self):
-        # this function turn the main window transparent and solid in alternation; it is a toggle function which uses
-        # self.counter to know if the window needs to be opaque or solid
-        if self.counter%2==0: # if pressing button to mke transparent
-            opacIntList=[] # create empty list for holding opacity float values
-            for num in range(4,10): # loop for filling list with needed opacity float values
-                opacInt = num / 10 # eg: 4/10 = 0.4
-                opacIntList.append(opacInt) # append to list [0.4,0.5,0.6...]
-            # self.master.wm_attributes('-transparentcolor', '#2a2b2b')
-            # self.master.wm_attributes('-transparentcolor', '#7E050C')
-            for num in range(1,6): # loop for incrementally increasing opacity
-                # pop values from list thus getting values from high to low
-                self.master.attributes('-alpha', opacIntList.pop()) # attributes with alpha on the main window changes opacity of whole window
-                time.sleep(0.1)
-            self.setCount()
-        else: # if pressing the same button to go back to regular solid colour
-            self.master.attributes('-alpha', 1.0)
-            self.setCount()
-    def screenShot(self):
-
-        if mainWin.selected.get() == '1':
-            screenShot=ScreenShot('f','LA','Feb_1','h')
-            screenShot.takeShot()
-        if mainWin.selected.get() == '2':
-            screenShot = ScreenShot('f', 'p', 'Feb_1','h')
-            screenShot.takeShot()
-
-
-    def createNew(self):
-
-        def click():
-            pass
-
-        text = Text(self.frame2, height=1)
-        text.pack()
-
-        text.insert('1.0', 'Set up click or text?')
-
-        self.button = Button(self.frame2, text="Click", width=12, command=click)
-        self.button.pack()
-
-        self.addItemButton = Button(self.frame2, text="Text", width=12, command=self.createNew)
-        self.addItemButton.pack()
+def close():
+    root.destroy()
 
 def main():
-    global mainWin  # Global mainWin so as to access the mainWin from functions which may need to call method
-    root = Tk()
-    mainWin = MainWindow(root)
+    global root
+    global mainWin
+    root=Tk()
+    mainWin=MainWindow(root)
 
     root.mainloop()
 
-
+main()
